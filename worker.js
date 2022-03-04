6368da19705e913066d3e0d7fce3fbd4fe2f396a
Kyoraku2 <tayebhakkar@yahoo.fr>
2022-03-04 12:09:21 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -59,7 +59,7 @@ self.addEventListener('fetch', (e) => {
       const response = await fetch(e.request);
       const cache = await caches.open(cacheName);
       //console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      //cache.put(e.request, response.clone());
+      cache.put(e.request, response.clone());
       return response;
     })());
   });
