f79637f9b4df2f6b518b4f495d1cef50e1951020
Kyoraku2 <tayebhakkar@yahoo.fr>
2022-03-15 16:36:08 +0100

    Update first step

--- worker.js
+++ worker.js
@@ -1,5 +1,4 @@
-var STATIC_CACHE_VERSION = '3';
-const cacheName = 'GolfuX';
+const cacheName = 'GolfuX-v1';
 const ASSETS = [
     '/textures/ball.png',
     '/textures/ball_pink.png',
@@ -55,19 +54,13 @@ for(var i = 1 ; i<=29 ; ++i){
 
 const contentToCache = ASSETS.concat(levels);
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys()
-      .then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== 'static-' + STATIC_CACHE_VERSION) {
-            console.log("update");
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
-  return self.clients.claim();
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }))
+  }));
 });
 
 self.addEventListener('install', (e) => {
@@ -76,7 +69,6 @@ self.addEventListener('install', (e) => {
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: GolfuX files');
       await cache.addAll(contentToCache);
-      console.log(contentToCache);
     })());
 });
 
@@ -84,12 +76,12 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
     e.respondWith((async () => {
       const r = await caches.match(e.request);
-      //console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
       if (r) { return r; }
       if(!(e.request.url.startsWith('http'))){return;}
       const response = await fetch(e.request);
       const cache = await caches.open(cacheName);
-      //console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
     })());
