46baf6e859b794a6f362b5050df40644e3fd513a
Kyoraku2 <tayebhakkar@yahoo.fr>
2022-03-15 17:14:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ if('serviceWorker' in navigator){
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-    alert("helo");
+    alert("helobis");
     /* Join by link */
     var gameId = (window.location.href.split("?").length == 2 && window.location.href.split("?")[1].match("gameId\=.*")) ? window.location.href.split("?")[1].split('=')[1] : "";
     if(gameId){
